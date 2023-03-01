f02c7a05128421427a85f15cb344f0ae1dacc37c
Yu Huang <yu.huang2018@gmail.com>
2023-02-28 21:29:14 -0800

    fixed server.js to launch homePage initially

--- front-end-webApp/server.js
+++ front-end-webApp/server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname + "/index.html"));
+    res.sendFile(path.join(__dirname + "/homePage.html"));
 })
 
 const server = app.listen(8084);
